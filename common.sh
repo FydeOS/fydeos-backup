ac0d6e7bba0b5fd91e35a15c199deb845bc25680
Fangzhou Chen <fangzhou@fydeos.io>
2023-03-23 17:46:12 +0800

    backup

---
+++ common.sh
@@ -0,0 +1,238 @@
+#!/usr/bin/env bash
+
+SCRIPT_ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+readonly SCRIPT_ROOT_DIR
+
+source "$SCRIPT_ROOT_DIR/log.sh"
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+CHROME_PROFILE_SUBDIR_NAME="user"
+readonly CHROME_PROFILE_SUBDIR_NAME
+ANDROID_DATA_SUBDIR_NAME="root/android-data"
+readonly ANDROID_DATA_SUBDIR_NAME
+
+INTERMEDIATE_BACKUP_RESTORE_FILE_PATH="/mnt/stateful_partition/encrypted/chronos/.fydeos_backup"
+readonly INTERMEDIATE_BACKUP_RESTORE_FILE_PATH
+
+PREFERENCE_JSON_FILE_NAME="Preferences"
+readonly PREFERENCE_JSON_FILE_NAME
+
+MY_FILES_PATH_NAME="MyFiles"
+readonly MY_FILES_PATH_NAME
+DOWNLOADS_PATH_NAME="Downloads" # same with "MyFiles/Downloads", do not clean it or backup it
+readonly DOWNLOADS_PATH_NAME
+
+GPG_HOMEDIR="/tmp/fydeos_backup/gnupg"
+readonly GPG_HOMEDIR
+GPG_BIN="gpg --homedir $GPG_HOMEDIR"
+readonly GPG_BIN
+
+CURRENT_USER_BASE_PATH=""
+CURRENT_USER_CHROME_DATA_DIR=""
+CURRENT_USER_ANDROID_DATA_DIR=""
+
+prepare_gpg() {
+  mkdir -p "$GPG_HOMEDIR"
+  chmod 700 "$GPG_HOMEDIR"
+  $GPG_BIN -k > /dev/null 2>&1 || { fatal "Failed to prepare gpg"; }
+}
+
+parse_email_from_preference_file() {
+ local file="$1"
+ if [[ -f "$file" ]]; then
+   jq -r '.account_info[0].email' "$file" || { error "Failed to parse email from preference file: $file"; echo ""; }
+ else
+   error "Unable to find email from preference file: $file"
+   echo ""
+ fi
+}
+
+get_hash_from_email() {
+ local email="$1"
+ cryptohome --action=obfuscate_user --user="$email" || echo ""
+}
+
+assert_current_mount_status() {
+  if ! findmnt "/home/chronos/user" -o SOURCE | grep -q shadow || [[ ! -f "/home/chronos/user/${PREFERENCE_JSON_FILE_NAME}" ]]; then
+    fatal "No user mounted at /home/chronos/user, cannot backup or restore"
+  fi
+}
+
+get_current_user_hash_id() {
+  findmnt -T /home/chronos/user -o SOURCE | grep shadow | awk -F/ '{print $6}'
+}
+
+get_current_user_base_path() {
+  if [[ -z "$CURRENT_USER_BASE_PATH" ]]; then
+    local id=""
+    id=$(get_current_user_hash_id)
+    CURRENT_USER_BASE_PATH="/home/.shadow/${id}/mount"
+  fi
+  echo "$CURRENT_USER_BASE_PATH"
+}
+
+get_current_user_chrome_profile_data_path() {
+  if [[ -z "$CURRENT_USER_CHROME_DATA_DIR" ]]; then
+    CURRENT_USER_CHROME_DATA_DIR="$(get_current_user_base_path)/${CHROME_PROFILE_SUBDIR_NAME}"
+  fi
+  echo "$CURRENT_USER_CHROME_DATA_DIR"
+}
+
+get_current_user_android_data_path() {
+  if [[ -z "$CURRENT_USER_ANDROID_DATA_DIR" ]]; then
+    CURRENT_USER_ANDROID_DATA_DIR="$(get_current_user_base_path)/${ANDROID_DATA_SUBDIR_NAME}"
+  fi
+  echo "$CURRENT_USER_ANDROID_DATA_DIR"
+}
+
+# get the email of current logged in user
+get_current_user_email() {
+  local path=""
+  path=$(get_current_user_chrome_profile_data_path)
+  local email=""
+  email=$(parse_email_from_preference_file "$path/${PREFERENCE_JSON_FILE_NAME}")
+  local hash_from_email=""
+  debug "email: $email"
+  hash_from_email=$(get_hash_from_email "$email")
+  local hash_from_findmnt=""
+  hash_from_findmnt=$(get_current_user_hash_id)
+  if [[ ! "$hash_from_findmnt" = "$hash_from_email" ]]; then
+    debug "hash_from_findmnt: $hash_from_findmnt, hash_from_email: $hash_from_email"
+    fatal "Unable to find the correct email of current logged in user"
+  fi
+  echo "$email"
+}
+
+verify_cryptohome_password() {
+  local u="$1"
+  local p="$2"
+  if ! cryptohome --action=check_key_ex --password="$p" --user="$u" > /dev/null; then
+    fatal "Unable to verify cryptohome password"
+  fi
+  info "Verified cryptohome password"
+}
+
+generate_key_for_backup_file() {
+  local u="$1"
+  local p="$2"
+  echo -n "$u:$p" | sha1sum | awk '{print $1}' | cut -c -16
+}
+
+kb2gb() {
+  local kb="$1"
+  echo "scale=2; $kb / 1024 / 1024" | bc | awk '{printf "%.2f", $0}'
+}
+
+get_folder_size() {
+  local d="$1"
+  if [[ ! -d "$d" ]]; then
+    echo "0"
+    return
+  fi
+  du -sk "$d" | awk '{print $1}'
+}
+
+get_available_space() {
+  local p="$1"
+  df -lk "$p" | grep "$p" | awk '{print $4}'
+}
+
+print_files_to_backup_disk_usage() {
+  local chrome_profile_size=0
+  local android_data_size=0
+  local available_space=0
+  chrome_profile_size="$(get_folder_size "$(get_current_user_chrome_profile_data_path)")"
+  android_data_size="$(get_folder_size "$(get_current_user_android_data_path)")"
+  local total_backup_size=0
+
+  total_backup_size=$(echo "$chrome_profile_size + $android_data_size" | bc)
+
+  available_space="$(get_available_space "/mnt/stateful_partition")"
+
+  echo "Total backup size: $(kb2gb "$total_backup_size")GB (Chrome profile size: $(kb2gb "$chrome_profile_size")GB, Android data size: $(kb2gb "$android_data_size")GB)"
+
+  echo "Available space: $(kb2gb "$available_space")GB"
+}
+
+generate_metadata_for_backup_file() {
+  local email="$1"
+  local timestamp="$2"
+  local file="$3"
+  local content=""
+  local checksum=""
+  checksum=$(echo -n "${email}:${timestamp}" | sha256sum | awk '{print $1}')
+  content="$(cat << EOF
+{
+  "checksum": "$checksum",
+  "datetime": "$timestamp"
+}
+EOF
+)"
+  echo "$content" > "${file}"
+}
+
+tar_backup_files() {
+  local email="$1"
+  local pass="$2"
+  local base_dir=""
+  base_dir=$(get_current_user_base_path)
+  echo "Tar backup files ${base_dir}/${CHROME_PROFILE_SUBDIR_NAME} and ${base_dir}/${ANDROID_DATA_SUBDIR_NAME}"
+  local filename=""
+  local datetime=""
+  datetime="$(date +%Y%m%d_%H%M%S)"
+  filename="fydeos_backup_${datetime}.tar.gz.gpg"
+
+  local dst="/home/chronos/user/Downloads"
+  local final="$dst/${filename}"
+
+  local tmp="${INTERMEDIATE_BACKUP_RESTORE_FILE_PATH}/${filename}"
+  mkdir -p "${INTERMEDIATE_BACKUP_RESTORE_FILE_PATH}"
+  echo "Backup the file to $final"
+  local temp_meta_dir=""
+  temp_meta_dir=$(mktemp -d "/tmp/fydeos_backup_XXXXXXXX") || fatal "Failed to create temporary directory"
+  local meta_file="backup_meta.json"
+  local meta_file_path="$temp_meta_dir/$meta_file"
+  generate_metadata_for_backup_file "$email" "$timestamp" "$meta_file_path"
+
+  # shellcheck disable=SC2064
+  trap "rm -f $tmp; rm -f $meta_file_path; rmdir $temp_meta_dir" SIGINT SIGTERM
+  # tar might return non-zero exit code due to files changes or some other reasons
+  local key=""
+  key=$(generate_key_for_backup_file "$email" "$pass")
+  debug "password for encryped backup file: $key"
+  set +o pipefail
+  if [[ "$WITH_MY_FILES" = "false" ]]; then
+    tar --preserve-permissions \
+      -czvf - \
+      -C "${temp_meta_dir}" \
+      "$meta_file" \
+      -C "${base_dir}" \
+      --exclude "${CHROME_PROFILE_SUBDIR_NAME}/${MY_FILES_PATH_NAME}" \
+      --exclude "${CHROME_PROFILE_SUBDIR_NAME}/${DOWNLOADS_PATH_NAME}" \
+      "${CHROME_PROFILE_SUBDIR_NAME}" \
+      "${ANDROID_DATA_SUBDIR_NAME}" 2> /dev/null \
+      | $GPG_BIN -v --passphrase "$key" -c -o "${tmp}"
+  else
+    #always exclude DOWNLOADS_PATH_NAME(Downloads) folder, even if myfiles is enabled, since downloads folder are the same with MyFiles/Downloads
+    tar --preserve-permissions \
+      -czvf - \
+      -C "${temp_meta_dir}" \
+      "$meta_file" \
+      -C "${base_dir}" \
+      --exclude "${CHROME_PROFILE_SUBDIR_NAME}/${DOWNLOADS_PATH_NAME}" \
+      "${CHROME_PROFILE_SUBDIR_NAME}" \
+      "${ANDROID_DATA_SUBDIR_NAME}" 2> /dev/null \
+      | $GPG_BIN -v --passphrase "$key" -c -o "${tmp}"
+  fi
+  sync
+  set -o pipefail
+
+  mv "${tmp}" "${final}"
+
+  chown chronos:chronos "$final"
+
+  echo "Tar backup files done, find the file $filename in Downloads folder"
+}
---
+++ main.sh
@@ -0,0 +1,201 @@
+#!/usr/bin/env bash
+
+SCRIPT_ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+source "$SCRIPT_ROOT_DIR/common.sh"
+
+set -o errexit
+
+ACTION_BACKUP="backup"
+readonly ACTION_BACKUP
+ACTION_RESTORE="restore"
+readonly ACTION_RESTORE
+
+RESTORE_MODE_MERGE="merge"
+readonly RESTORE_MODE_MERGE
+RESTORE_MODE_REPLACE="replace"
+readonly RESTORE_MODE_REPLACE
+DEFAULT_RESTORE_MODE="${RESTORE_MODE_MERGE}"
+readonly DEFAULT_RESTORE_MODE
+
+ACTION=""
+RESTORE_MODE=""
+BACKUP_FILE=""
+WITH_MY_FILES="false"
+CREATE_NEW_USER="false"
+USER_EMAIL=""
+PASSWORD=""
+
+usage() {
+  cat <<EOF
+Usage: $0 [backup|restore] [OPTIONS]
+
+Commands:
+  backup              Perform a backup of the data
+  restore             Restore the data from a backup file
+
+Options for backup:
+  --with-myfiles      Include 'My Files' in the backup
+  --password <pass>   Specify the password to verify user identity and encrypt the backup file
+                      Please note that it is not recommended to specify the password directly in the command line parameters. This script will prompt the user to enter the password when needed
+  -d, --debug         Enable debug mode
+
+Options for restore:
+  --restore-mode <mode>   Specify the restore mode: ${RESTORE_MODE_MERGE} or ${RESTORE_MODE_REPLACE} (default: ${DEFAULT_RESTORE_MODE})
+  -f, --file <file>       Specify the backup file to restore from
+  -n, --new <email>       Create a new user with a given email and restore data for that user
+  --password <pass>       Specify a password for new user's encrypted directory and decrypting backup file
+                          Please note that it is not recommended to specify the password directly in the command line parameters. This script will prompt the user to enter the password when needed
+  -d, --debug             Enable debug mode
+
+-h, --help            Display this help message and exit
+EOF
+  exit "${1:-0}"
+}
+
+prompt_for_password() {
+  if [[ -n "$PASSWORD" ]]; then
+    warn "Setting password in command line arguments is not recommended. The script will prompt for your password."
+    return
+  fi
+  local prompt="Enter Password:"
+  local email=""
+  if [[ "$ACTION" = "$ACTION_BACKUP" ]]; then
+    prompt="The current logged-in user is $USER_EMAIL, please enter the login password to verify your identity, and the password will be used to encrypt the backup file:"
+  elif [[ "$ACTION" = "$ACTION_RESTORE" ]] && [[ "$CREATE_NEW_USER" = "true" ]]; then
+    prompt="Please enter the password for decrypting the backup file and create encrypted directory for the new user:"
+  fi
+  while IFS= read -p "$prompt" -r -s -n 1 char; do
+    if [[ $char == $'\0' ]]; then
+      break
+    fi
+    prompt='*'
+    PASSWORD+="$char"
+  done
+  echo
+}
+
+print_params() {
+  debug "ACTION: $ACTION"
+  debug "RESTORE_MODE: $RESTORE_MODE"
+  debug "BACKUP_FILE: $BACKUP_FILE"
+  debug "WITH_MY_FILES: $WITH_MY_FILES"
+  debug "CREATE_NEW_USER: $CREATE_NEW_USER"
+  debug "USER_EMAIL: $USER_EMAIL"
+  debug "PASSWORD: $PASSWORD"
+}
+
+verify_params() {
+  if [[ ! $ACTION = "$ACTION_RESTORE" ]] && [[ ! "$ACTION" = "$ACTION_BACKUP" ]]; then
+    fatal "Invalid action: $ACTION"
+  fi
+  if [[ ! $ACTION = "$ACTION_RESTORE" ]] && [[ -n "$RESTORE_MODE" ]]; then
+    info "Restore mode will be ignored for $ACTION action"
+  fi
+  if [[ "$ACTION" = "$ACTION_RESTORE" ]]; then
+    if [[ -z "$RESTORE_MODE" ]]; then
+      RESTORE_MODE="${DEFAULT_RESTORE_MODE}"
+    fi
+    if [[ "$RESTORE_MODE" != "${RESTORE_MODE_MERGE}" ]] && [[ "$RESTORE_MODE" != "$RESTORE_MODE_REPLACE" ]]; then
+      fatal "Invalid restore mode: $RESTORE_MODE. Valid modes are ${RESTORE_MODE_MERGE} or ${RESTORE_MODE_REPLACE}."
+    fi
+
+    if [[ -z "$BACKUP_FILE" ]]; then
+      fatal "Backup file is required for $ACTION action"
+    fi
+    if [[ "$CREATE_NEW_USER" = "true" ]] && [[ -z "$USER_EMAIL" ]]; then
+      fatal "User email is required for creating new user and restore data"
+    fi
+  fi
+}
+
+do_backup() {
+  USER_EMAIL=$(get_current_user_email)
+  prompt_for_password
+  verify_cryptohome_password "$USER_EMAIL" "$PASSWORD"
+
+  print_files_to_backup_disk_usage
+
+  tar_backup_files "$USER_EMAIL" "$PASSWORD"
+}
+
+main() {
+  while [[ $# -gt 0 ]]; do
+    key="$1"
+    case "$key" in
+      backup)
+        ACTION="backup"
+        shift
+        while [[ $# -gt 0 ]]; do
+          case "$1" in
+            --with-myfiles)
+              WITH_MY_FILES="true"
+              shift
+              ;;
+            --password)
+              PASSWORD="$2"
+              shift
+              shift
+              ;;
+            -d|--debug)
+              LOG_LEVEL="debug"
+              shift
+              ;;
+            *)
+              usage 1
+              ;;
+          esac
+        done
+        ;;
+      restore)
+        ACTION="restore"
+        shift
+        while [[ $# -gt 0 ]]; do
+          case "$1" in
+            --restore-mode)
+              RESTORE_MODE="$2"
+              shift
+              shift
+              ;;
+            -f|--file)
+              BACKUP_FILE="$2"
+              shift
+              shift
+              ;;
+            -n|--new)
+              CREATE_NEW_USER="true"
+              USER_EMAIL="$2"
+              shift
+              shift
+              ;;
+            --password)
+              PASSWORD="$2"
+              shift
+              shift
+              ;;
+            -d|--debug)
+              LOG_LEVEL="debug"
+              shift
+              ;;
+            *)
+              usage 1
+              ;;
+          esac
+        done
+        ;;
+      -h|--help)
+        usage
+        ;;
+      *)
+        usage 1
+        ;;
+    esac
+  done
+  print_params
+
+  prepare_gpg
+  if [[ "$ACTION" = "$ACTION_BACKUP" ]]; then
+    do_backup
+  fi
+}
+
+main "$@"
